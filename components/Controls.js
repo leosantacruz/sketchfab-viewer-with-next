78ac77d0be711be02193e6175d9b8de402c3fe31
Leonardo Santa Cruz <leo@MacBook-Pro-de-Leonardo.local>
2022-03-11 13:14:41 -0300

    Mapping buttons in Controls, Navbar logo links updated

--- components/Controls.js
+++ components/Controls.js
@@ -2,13 +2,19 @@ import { useState } from "react"
 export default function Controls({ apiRef, materialList }) {
     const [darkBackground, setDarkBackground] = useState()
     const [selectedMaterial, setSelectedMaterial] = useState()
+    const colorOptions = [
+        { name: "Aqua", color: [32, 177, 166], className: "accent" },
+        { name: "Pink", color: [216, 39, 169], className: "secondary" },
+        { name: "Blue", color: [58, 190, 248], className: "info" },
+        { name: "Yellow", color: [251, 188, 35], className: "warning" },
+    ]
 
     const defaultCamaraPosition = {
         position: [-0.07422821498173426, -0.667373392962512, 0.6363603975021233],
         target: [0.2221558569363637, -0.04370469737132048, -0.03924365014719243]
     }
 
-    const changeColor = (color) => {
+    const changeMaterialColor = (color) => {
         materialList[selectedMaterial - 1].channels.AlbedoPBR.color = [color[0] / 255, color[1] / 255, color[2] / 255, 1]
         apiRef.current.setMaterial(materialList[selectedMaterial - 1], () => {
             console.log("Updated chair color");
@@ -56,19 +62,14 @@ export default function Controls({ apiRef, materialList }) {
                     </ul>
                 }
             </div>
-            <div className="grid grid-cols-2 gap-3 md:gap-0">
-                <button className="btn md:col-span-2 col-span-1 mb-3 justify-start" onClick={() => changeGroupColor([32, 177, 166])}>  <div className="badge badge-accent mr-4"></div>Aqua
-                </button>
-                <button className="btn md:col-span-2 col-span-1 mb-3 justify-start" onClick={() => changeGroupColor([216, 39, 169])}>  <div className="badge badge-secondary mr-4"></div>Pink
-                </button>
-                <button className="btn md:col-span-2 col-span-1 mb-3 justify-start" onClick={() => changeGroupColor([58, 190, 248])}>  <div className="badge badge-info mr-4"></div>Blue
-                </button>
-                <button className="btn md:col-span-2 col-span-1 mb-3 justify-start" onClick={() => changeGroupColor([251, 188, 35])}>  <div className="badge badge-warning mr-4"></div>Yellow
-                </button>
+            <div className="grid grid-cols-2 gap-3 md:gap-0x">
+                {colorOptions.map(option => {
+                    return <button className="btn md:col-span-2 col-span-1 mb-3 justify-start" onClick={() => changeGroupColor(option.color)}>  <div className={`badge badge-${option.className} mr-4`}></div>{option.name}
+                    </button>
+                })}
             </div>
             <button className="btn btn-block mb-3" onClick={setCameraPosition}>Close up</button>
             <div className="text-center">
-
                 <label className="swap swap-rotate">
                     <input type="checkbox" onClick={toggleDarkMode} />
                     <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,11 @@ export default function Navbar() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <img height="50" width="60" src="https://ibisdev.tech/images/logo-white.svg" alt="Ibisdev logo" className="mx-5" />
-                <img width="140" src="https://static.sketchfab.com/img/press/logos/sketchfab-logo-text-white.svg" alt="Sketchfab logo" />
+                <a target="_blank" href="https://ibisdev.tech/"><img height="50" width="60" src="https://ibisdev.tech/images/logo-white.svg" alt="Ibisdev logo" className="mx-5" /></a>
+                <a target="_blank" href="https://sketchfab.com/blogs/enterprise/partners/ibisdev"><img width="140" src="https://static.sketchfab.com/img/press/logos/sketchfab-logo-text-white.svg" alt="Sketchfab logo" /></a>
             </div>
             <div className="flex-none">
-                <a href="https://ibisdev.tech/" className="btn btn-primary btn-xs md:btn-sm">Visit our website</a>
+                <a href="https://ibisdev.tech/#contact-us" className="btn btn-primary btn-xs md:btn-sm">Contact us</a>
             </div>
         </div>
     )
