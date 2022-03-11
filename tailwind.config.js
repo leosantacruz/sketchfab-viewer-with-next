c982f80f0c6f3069e63954ba1f2f93deb6473e6e
Leonardo Santa Cruz <leo@MacBook-Pro-de-Leonardo.local>
2022-03-11 15:33:57 -0300

    Allowing dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     plugins: [require("daisyui")],
     daisyui: {
         styled: true,
-        themes: true,
+        themes: ["dark"],
         base: true,
         utils: true,
         logs: true,
